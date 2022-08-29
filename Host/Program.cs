3fc31f7b7fecea435a7e9710d9e3a1cb46c5b60f
fizrahmanov <FaizrahmanovAF@ad.atlant.by>
2022-08-29 11:11:21 +0300

    Вручную прописана зависимость на Models в Host.csproj.

--- Host/Program.cs
+++ Host/Program.cs
@@ -1,8 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-
+using Models;
 
 Console.WriteLine("Start App");
 Console.WriteLine();
 
-Domestication Cow = new Domestication("Cow", 10, 100);
+Cow.Eat();
